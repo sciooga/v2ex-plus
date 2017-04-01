e34744c29e04800022c79cda73ebcc2d3c00242f
gloomy-ghost <gloomy-ghost@users.noreply.github.com>
2017-04-01 10:42:31 +0000

    Fix preview

--- v2ex_tab.js
+++ v2ex_tab.js
@@ -34,7 +34,7 @@
                 _this.text('加载中...');
                 _cell.after("<div class='previewWindow'></div>");
                 var _href = _cell.find('.item_title a').attr('href');
-                $.get(_href, function(data){
+                $.get('https://www.v2ex.com'+_href, function(data){
                     var _previewWindow = _cell.next('.previewWindow');
                     data = data.substr(data.indexOf("header")+8);
                     //当匹配不到topic_conten时返回-1 substring 按0处理，既显示整个标题头部
