1c5154f5f124bfb9838a7061275ec93900503a33
p0we7 <i@iu.vc>
2021-08-15 17:19:41 +0800

    improve: custom nodes are turned off by default

--- page/options.js
+++ page/options.js
@@ -68,7 +68,7 @@ const defaultSettings = {
     "replyA": 0.4,
     "thankColor": "#cccccc",
     "sov2ex": 0,
-    "customNode": "www",
+    "customNode": "-1",
     "base64": 0,
     "darkTheme": 0,
     "imageParsing": "auto-hide",
