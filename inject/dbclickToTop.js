970f41c1588010123c4f5c763f4cd3625b7192b6
T <t@TdeMacBook-Pro.local>
2023-03-02 03:04:01 +0800

    功能：增加双击返回顶部 默认关闭

---
+++ inject/dbclickToTop.js
@@ -0,0 +1,8 @@
+chrome.storage.sync.get("options", (data) => {
+    if (data.options.dblclickToTop) {
+        document.addEventListener('dblclick', e => {
+            window.getSelection().removeAllRanges();
+            document.body.scrollIntoView()
+        })
+    }
+})
